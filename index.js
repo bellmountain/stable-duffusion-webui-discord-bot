b1a099ea5f1bfde64fd551eb47f6b9c18426194b
N <d.kroeger@bellmountain-media.de>
2023-07-12 23:18:36 +0200

    changed default value of sharePrompt to true

--- index.js
+++ index.js
@@ -1,73 +1,89 @@
-import { Client, IntentsBitField, EmbedBuilder, AttachmentBuilder } from 'discord.js'
+import {
+  Client,
+  IntentsBitField,
+  EmbedBuilder,
+  AttachmentBuilder
+} from 'discord.js'
 import env from 'dotenv'
 import generateImage from './api/txt2img.js'
 import { Buffer } from 'node:buffer'
 
-const negative = 'bad anatomy,bad proportions,blurry,cloned face,cropped,deformed,dehydrated,disfigured,duplicate,error,extra arms,extra fingers,extra legs,extra limbs,fused fingers,gross proportions,jpeg artifacts,long neck,low quality,lowres,malformed,limbs missing,arms missing,legs missing,morbid,mutated hands,mutation,mutilated,out of frame,poorly drawn face,poorly drawn hands,signature,too many fingers,ugly,username,watermark,worst quality'
+const negative =
+  'bad anatomy,bad proportions,blurry,cloned face,cropped,deformed,dehydrated,disfigured,duplicate,error,extra arms,extra fingers,extra legs,extra limbs,fused fingers,gross proportions,jpeg artifacts,long neck,low quality,lowres,malformed,limbs missing,arms missing,legs missing,morbid,mutated hands,mutation,mutilated,out of frame,poorly drawn face,poorly drawn hands,signature,too many fingers,ugly,username,watermark,worst quality'
 env.config()
- 
+
 const client = new Client({
-    intents : [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-        IntentsBitField.Flags.DirectMessages,
-    ] 
-}) 
-client.on('ready',(c)=>{
-    console.log(`${c.user.tag} is online`)
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.DirectMessages
+  ]
 })
-client.on('interactionCreate', async (interaction)=> {
+client.on('ready', c => {
+  console.log(`${c.user.tag} is online`)
+})
+client.on('interactionCreate', async interaction => {
+  if (interaction.commandName === 'txt2img') {
+    try {
+      const prompt = interaction.options.get('prompt').value
+      const negativePrompt = interaction.options.get('negative_prompt')
+        ? interaction.options.get('negative_prompt').value
+        : negative
+      const seed = interaction.options.get('seed')
+        ? interaction.options.get('seed').value
+        : undefined
+      const amount = interaction.options.get('amount')
+        ? interaction.options.get('amount').value
+        : 1
+      const sharePrompt = interaction.options.get('share_prompt')
+        ? interaction.options.get('share_prompt').value
+        : true
 
-      if(interaction.commandName === 'txt2img') {
-     try { 
-          const prompt = interaction.options.get('prompt').value
-          const negativePrompt = interaction.options.get('negative_prompt') ? interaction.options.get('negative_prompt').value : negative 
-          const seed = interaction.options.get('seed') ? interaction.options.get('seed').value : undefined
-          const amount = interaction.options.get('amount') ? interaction.options.get('amount').value : 1 
-          const sharePrompt = interaction.options.get('share_prompt') ? interaction.options.get('share_prompt').value : false 
+      await interaction.deferReply()
+      const imgData = await generateImage({
+        prompt: prompt,
+        negativePrompt: negativePrompt,
+        seed: seed,
+        amount: amount
+      })
+      //console.log(typeof imgData)
+      //console.log(data)
 
-          await interaction.deferReply()
-          const imgData = await generateImage({
-            prompt: prompt,
-            negativePrompt: negativePrompt,
-            seed: seed,
-            amount: amount
-          })
-          //console.log(typeof imgData)
-          //console.log(data)
+      //base64 string ->
+      // Convert the base64 to a buffer
+      //const buffer = Buffer.from(imgData, 'base64');
+      const images = imgData.images.map(
+        img => new AttachmentBuilder(Buffer.from(img, 'base64'), 'test')
+      )
+      const jsonInfo = JSON.parse(imgData.info)
 
-          //base64 string ->
-          // Convert the base64 to a buffer
-          //const buffer = Buffer.from(imgData, 'base64');
-          const images = imgData.images.map(
-            img=>new AttachmentBuilder( Buffer.from(img,'base64') , 'test' ) 
-          )
-          const jsonInfo = JSON.parse( imgData.info )
-          
-          console.log(jsonInfo)
+      console.log(jsonInfo)
 
-          //console.log(image)
-          /*
+      //console.log(image)
+      /*
           const embed = new EmbedBuilder()
             .setTitle('Embed')
             .setDescription('description')
             //.setColor('')
             .addFields({ name: 'Field title', value: 'some value' })
           */
-          //console.log(image)
-          //interaction.reply(prompt + negativePrompt)
-          //interaction.reply({embeds: [embed], files: [image]})
-          interaction.followUp({content: `Seed: ${jsonInfo.seed.toString()}${sharePrompt? '\nPrompt: ' + prompt : ''}`,
-          files: images})
-          //interaction.reply(image)
-          //console.log(prompt)
-    } catch(ex) {
+      //console.log(image)
+      //interaction.reply(prompt + negativePrompt)
+      //interaction.reply({embeds: [embed], files: [image]})
+      interaction.followUp({
+        content: `Seed: ${jsonInfo.seed.toString()}${
+          sharePrompt ? '\nPrompt: ' + prompt : ''
+        }`,
+        files: images
+      })
+      //interaction.reply(image)
+      //console.log(prompt)
+    } catch (ex) {
       console.log(ex)
     }
-      }
-
+  }
 })
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
